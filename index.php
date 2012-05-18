b78ddec8681ddf2d95ae78c386750eeef738cc82
Lucas Tadeu Teixeira <lucas.tadeuteixeira@gmail.com>
2012-05-18 18:23:25 -0300

    info adicionada

--- index.php
+++ index.php
@@ -5,13 +5,40 @@
 	<title> Facebook Archiver - Main </title>
 
 	<link rel="stylesheet" href="css/style.css" media="screen" />
-	<script src="js/fbarchiver.js"></script>
+
+
+
+	<script src="js/fbarchiver.js"></script>	
 
 </head>
 
 <body>
 
 	<div id="wraper">
+
+	<div id="fb-root"></div>
+	<!-- Starts up facebook's JS API -->
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '411476732220632', // App ID
+        status     : true, // check login status
+        cookie     : true, // enable cookies to allow the server to access the session
+        xfbml      : true  // parse XFBML
+      });
+    };
+
+    // Load the SDK Asynchronously
+    (function(d){
+      var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+      js = d.createElement('script'); js.id = id; js.async = true;
+      js.src = "//connect.facebook.net/en_US/all.js";
+      d.getElementsByTagName('head')[0].appendChild(js);
+    }(document));
+  </script>
+
+	<fb:login-button show-faces="true" width="200" max-rows="1" scope="publish_actions">
+	  </fb:login-button>
 
 		<div id="header">
 			
@@ -28,9 +55,8 @@
 			<div id="posts">
 				
 				<div class="post">
-					<span class="date">05-18-2012</span>
+					<span class="date"></span>
 					<blockquote class="post-data">
-						Some random data.
 					</blockquote>
 				</div>
 
